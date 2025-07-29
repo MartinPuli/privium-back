328b91ed5b44c12544ec33ccf8b8471d0b281d3d
MartinPuli <101229882+MartinPuli@users.noreply.github.com>
2025-07-29 01:39:56 -0300

    Add endpoint to fetch user by id

--- src/main/java/Marketplace/services/UserService.java
+++ src/main/java/Marketplace/services/UserService.java
@@ -1,7 +1,9 @@
 package Marketplace.services;
 
+import Marketplace.commons.dtos.ResponseDataDto;
 import Marketplace.commons.dtos.ResponseDto;
 import Marketplace.dtos.request.UserRequestDto;
+import Marketplace.models.User;
 
 import java.sql.SQLException;
 
@@ -9,4 +11,5 @@ public interface UserService {
     ResponseDto manageProfilePicture(Long userId, UserRequestDto request) throws SQLException;
     ResponseDto updateUser(Long userId, UserRequestDto request) throws SQLException;
     ResponseDto deleteUser(Long userId) throws SQLException;
+    ResponseDataDto<User> getUserById(Long userId) throws SQLException;
 }
