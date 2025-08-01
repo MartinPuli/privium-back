8bee793febf08d6b61f0d89b7c1bdba158cef323
martinezequiel.pulitano.contractor.gdevtools.com <MARTINEZEQUIEL.PULITANO.CONTRACTOR@GDEVTOOLS.COM>
2025-08-01 04:14:17 -0300

    error s3

--- src/main/java/Marketplace/config/S3Config.java
+++ src/main/java/Marketplace/config/S3Config.java
@@ -4,20 +4,42 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.regions.Region;
 
 @Configuration
 public class S3Config {
-  @Value("us-east-1")
-  private Region region;
 
-  @Bean
-  public S3Client s3Client() {
-    return S3Client.builder()
-      .region(region)
-      .credentialsProvider(ProfileCredentialsProvider.create("privium-dev"))
-      .build();
-  }
+    @Value("${aws.region}")
+    private String region;
+
+    @Value("${AWS_ACCESS_KEY_ID:}")
+    private String accessKey;
+
+    @Value("${AWS_SECRET_ACCESS_KEY:}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        // Si existen variables de entorno, usa StaticCredentialsProvider (Render)
+        if (accessKey != null && !accessKey.isEmpty()) {
+            return S3Client.builder()
+                    .region(Region.of(region))
+                    .credentialsProvider(
+                            StaticCredentialsProvider.create(
+                                    AwsBasicCredentials.create(accessKey, secretKey)
+                            )
+                    )
+                    .build();
+        }
+
+        // Si no existen, usa perfil local de AWS (~/.aws/credentials)
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(ProfileCredentialsProvider.create("privium-dev"))
+                .build();
+    }
 }
